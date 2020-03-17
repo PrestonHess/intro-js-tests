a36269f7acedb92ba0eb11b368b4c54d67035244
Preston Hess <hesspreston@outlook.com>
2020-03-16 22:44:11 -0700

    objects

--- app/objects.js
+++ app/objects.js
@@ -13,11 +13,9 @@ let goodDoggo = {
   likes: ['Bacon', 'Fetch', 'Tennis Balls', 'Digging in the garden']
 }
 
-
 // write a function that returns the value of the legs property on your dog object using dot notation.
 function howManyLegs() {
-  // your code inside here
-  // don't forget to use the return keyword
+  return goodDoggo.legs;
 }
 
 // Cat Challenges
@@ -34,6 +32,8 @@ let cat = {
 // the value of the name property.
 function renameCat() {
   // your code inside here
+  cat['name'] = "Kevin";
+  return cat.name;
 }
 
 // Create a function that uses dot or bracket notation to create a property 
@@ -42,15 +42,17 @@ function renameCat() {
 
 function colorTheCat() {
   // your code inside here
+  cat.color = "Black";
+  return cat.color;
 }
 
 // Delete the whiskers property off of the cat object. You may use either dot or bracket notation.
-
+delete cat.whiskers;
 
 // Write a function that uses dot AND bracket notation to access and return the 
 // value of the fourth item in the likes array("pets"). 
 // Example: objectName.property[indexNumber]
 
 function returnIndex(cat) {
-  // your code inside here
+  return cat.likes[3];
 }
