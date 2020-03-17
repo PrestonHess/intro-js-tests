259d044f899251b38e44e66cb46eb1fa1ec4c6d5
Preston Hess <hesspreston@outlook.com>
2020-03-16 23:06:04 -0700

    bonuses

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,10 @@
 let str = "team number 2"
 
 function camelCase(str) {
-
+  let words = str.split(' ');
+  let result = words[0];
+  for (let i = 1; i < words.length; i++) {
+    result += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return result;
 }
